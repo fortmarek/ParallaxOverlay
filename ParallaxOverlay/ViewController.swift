9138a6370b227cb55c64acdf8de5879fa1975bd4
Marek Fořt <marekfort@me.com>
2019-07-28 20:16:23 +0200

    Initial Commit

---
+++ ParallaxOverlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParallaxOverlay
+//
+//  Created by Marek Fořt on 7/28/19.
+//  Copyright © 2019 Marek Fořt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
